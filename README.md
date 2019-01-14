6d8e3801b0fc244ba4849d7601bb2e668cc5e658
joao.arvins <joao.arvins@gmail.com>
2019-01-14 21:31:09 +0000

    Added streams dynamo db table resource

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# video-consume
+# stream-checker
 
 POST https://ds5so3hsma.execute-api.eu-west-2.amazonaws.com/dev/stream-record
--- src/functions/stream.js
+++ src/functions/stream.js
@@ -1,13 +1,15 @@
 'use strict';
 
-module.exports.add = async (event, context) => {
-  return {
+module.exports.add = async (event, context, callback) => {
+  const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
     }),
   };
+
+  callback(null, response);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
