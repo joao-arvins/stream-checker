5f3e8c70c9b922e5799b09520a10977a0b07d4b7
joao.arvins <joao.arvins@gmail.com>
2019-01-17 22:34:55 +0000

    Logic to return 400 for invalid body

--- src/functions/stream.js
+++ src/functions/stream.js
@@ -4,33 +4,33 @@ const streamRecordService = require('../services/streamRecordService');
 const log = require('../lib/logger');
 
 module.exports.add = async (event, context, callback) => {
-  let { userId, streamId } = JSON.parse(event.body);
+  let body = JSON.parse(event.body);
 
-  if(typeof userId !== 'string' || typeof streamId !== 'string') {
+  if(typeof body.userId !== 'string' || typeof body.streamId !== 'string') {
     let invalidDataMessage = 'Invalid data. Payload must contain 2 required string fields: userId and streamId';
-    log.error(invalidDataMessage, { userId, streamId });
+    log.error(invalidDataMessage, body);
 
     callback(null, {
-      statusCode: 400,
-      message: 'Invalid data. Payload must contain 2 required string fields: userId and streamId'
+      statusCode: 400
     });
+    
     return;
   }
 
   try {
-    const recordAdded = await streamRecordService.addRecord(userId, streamId);
+    const recordAdded = await streamRecordService.addRecord(body.userId, body.streamId);
     
-    let message = `Adding stream record for user ${userId} and stream ${streamId}. 
+    let message = `Adding stream record for user ${body.userId} and stream ${body.streamId}. 
       Result: ${recordAdded ? 'Added' : 'Not Added'}`;
       
-    log.info(message, { userId, streamId });
+    log.info(message, body);
 
     callback(null, {
       statusCode: recordAdded ? 201 : 403
     });
   }
   catch(e) {
-    log.error('Internal Server Error', { userId, streamId }, e);
+    log.error('Internal Server Error', body, e);
     callback(null, {
       statusCode: 500,
       body: JSON.stringify(e)
--- tests/unit/functions/stream.test.js
+++ tests/unit/functions/stream.test.js
@@ -44,8 +44,24 @@ describe('Adding a new record stream', () => {
             output = callback.mock.calls[0][1];
         });
 
-        it('A 500 response code is returned', () => {
+        it('A 500 - Internal Server error response code is returned', () => {
             expect(output.statusCode).toEqual(500);
+        });
+    });
+
+    describe('When the request body is invalid', () => {
+        beforeEach(async () => {           
+            let body = JSON.stringify({ streamId : 1, userId: "test" });  
+            await add({ body }, {}, callback);
+            output = callback.mock.calls[0][1];
+        });
+
+        it('No request to create a stream record is performed', () => {
+            expect(addRecordSpy).not.toHaveBeenCalled();
+        });
+
+        it('A 400 - Bad request response code is returned', () => {
+            expect(output.statusCode).toEqual(400);
         });
     });
 });
