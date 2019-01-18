45441b0e7d7f5160d6401d709f47da382616f5b1
joao.arvins <joao.arvins@gmail.com>
2019-01-18 16:40:52 +0000

    Logic to cleanup records added during integration tests

--- tests/integration/add.test.js
+++ tests/integration/add.test.js
@@ -1,64 +1,98 @@
 const uuidv4 = require('uuid/v4');
 const { add } = require('../../src/functions/stream');
 const testPayload = require('../testPayload.json');
+const AWS = require('aws-sdk'); 
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 describe('Adding a new record stream', () => {
     let output, callback, userId, payload;
-
-    afterAll(() => {
-        //TODO: Delete created items
-    });
 
     beforeEach(() => {
         callback = jest.fn();
     });
 
     describe('When the given user has no active streams', () => {
-        beforeEach(async () => {
-            let userId = uuidv4();
-            testPayload.body = `{\"userId\" : \"${userId}\", \"streamId\" : \"1\"}`;
+        beforeEach(async (done) => {
+            testPayload.body = createBodyPayload();
 
             await add(testPayload, {}, callback);
 
             output = callback.mock.calls[0][1];
+
+            deleteTestItems(callback.mock.calls, done);
         });
 
         it('The new stream record is created', () => {
             expect(output.statusCode).toEqual(201);
+            expect(output.body).toBeDefined();
         });
     });
 
-    describe('When the number of active streams is lower than the maximum', () => {
-        beforeEach(async () => {
-            let userId = uuidv4();
-            testPayload.body = `{\"userId\" : \"${userId}\", \"streamId\" : \"1\"}`;
-
+        describe('When the number of active streams is lower than the maximum', () => {
+        beforeEach(async (done) => {
+            testPayload.body = createBodyPayload();
             for (let index = 0; index <= process.env.MAX_STREAMS_COUNT - 1; index++) {
                 await add(testPayload, {}, callback);
             }
 
             output = callback.mock.calls[process.env.MAX_STREAMS_COUNT - 1][1];
+
+            deleteTestItems(callback.mock.calls, done);
         });
 
         it('The new stream record is created', () => {
             expect(output.statusCode).toEqual(201);
+            expect(output.body).toBeDefined();
         });
     });
 
     describe('When the number of active streams equals the maximum amount', () => {
-        beforeEach(async () => {
-            let userId = uuidv4();
-            testPayload.body = `{\"userId\" : \"${userId}\", \"streamId\" : \"1\"}`;
+        beforeEach(async (done) => {
+            testPayload.body = createBodyPayload();
 
             for (let index = 0; index <= process.env.MAX_STREAMS_COUNT; index++) {
                 await add(testPayload, {}, callback);
             }
 
             output = callback.mock.calls[process.env.MAX_STREAMS_COUNT][1];
+
+            deleteTestItems(callback.mock.calls, done);
         });
 
         it('The new stream record is NOT created', () => {
             expect(output.statusCode).toEqual(403);
+            expect(output.body).toBeUndefined();
         });
     });
 });
+
+const createBodyPayload = () => {
+    let userId = uuidv4();
+    return `{\"userId\" : \"${userId}\", \"streamId\" : \"1\"}`;
+};
+
+//Deletes all items created during tests
+const deleteTestItems = (calls, callback) => {
+    const params = {
+        RequestItems: {
+          [process.env.STREAMS_TABLE]: []
+        }
+    };
+
+    params.RequestItems[process.env.STREAMS_TABLE] = calls
+        .filter(call => call[1].body)
+        .map(call => {
+            let newItemId = JSON.parse(call[1].body).id;
+            return {
+                DeleteRequest: {
+                  Key: { id: newItemId }
+                }
+            };
+        });
+
+    dynamoDb.batchWrite(params)
+        .promise()
+        .then(res => {
+            callback();
+        });
+}
